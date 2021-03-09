98e67cfdca308d49dbe3342b85eb876274481716
Piyush Priyadarshi <71820747+piyushpisu@users.noreply.github.com>
2021-03-09 17:43:27 +0530

    Update Project_1_SnakeGunWater.py

--- Project_1_SnakeGunWater.py
+++ Project_1_SnakeGunWater.py
@@ -1,40 +1,72 @@
-import random
+""" My First Project/Game #Snake Water and Gun Game!!!!"""
 
-def gameWin(computer,human):
-	if computer==human:
-		return None
-	elif computer=="s":
-		if human=="w":
-			return False
-		elif human=="g":
-			return True
-	elif computer=="w":
-		if human=="g":
-			return False
-		elif human=="s":
-			return True
-	elif computer=="g":
-		if human=="s":
-			return False
-		elif human=="w":
-			return True
-print("Computer turn snake(s=1)water(w=2)gun(g=2)-- ??")
+import random
+def GameWon(computer,human):
+if computer==human:
+	return None
+elif computer=="s" and human=="w":
+	return computer
+elif computer=='w' and human=='s':
+	return human
+elif computer=='g' and human=='w':
+	return human
+elif computer=='w' and human=='g':
+		return computer
+elif computer=='g' and human=='s':
+		return computer
+elif computer=='s' and human=='g':
+		return human
+
+computer_score=0
+
+human_score=0
+
+rounds=int(input("How Many Rounds You Want to Play - "))
+print("\n")
+
+for i in range(1,rounds+1):
+print("Computer Turn-snake(s=1)water(w=2)gun(g=2)-??\n")
+
 x=random.randint(1,3)
 if x==1:
-	computer ='s'
+	computer="s"
 elif x==2:
-	computer ='w'
+	computer='w'
 elif x==3:
-	computer ='g'
-human=input("Your Turn snake(s=1)water(w=2)gun(g=2)-- ")
-a = gameWin(computer,human)
-print(f"Computer choose- {computer}")
-print(f"Human choose- {human}")
-if a == None:
-	print("Game Tie")
-elif a== True:
-	print("You Won")
+	computer='g'
+
+human=input("Human Turn-snake(s=1)water(w=2)gun(g=2)- ",)
+if human =="w":
+	pass
+elif human =="s":
+	pass
+elif human =="g":
+	pass
 else:
-	print("Computer Won")
+	print("Please Choose a Value out of s,w,g")
+	break
 
+print(f"Computer Chooses- {computer}\n")
+print(f"Human Chooses- {human}\n")
 
+a = GameWon(computer,human)
+if a==None:
+	print("Round",i,"- Tie \n")
+elif a==human:
+	print("Round",i,"- Human Won \n")
+	human_score+=1
+elif a==computer:
+	print("Round",i,"- Computer Won \n")
+	computer_score+=1
+i+=1
+
+if i==rounds+1:
+		print(f"Computer Score is {computer_score}\n")
+		print(f"Human Score is {human_score}\n")
+
+		if computer_score>human_score:
+			print("Computer Won the Series by",computer_score,"-",human_score,"\n")
+		elif human_score>computer_score:
+			print("Human Won the Series by",human_score,"-",computer_score,"\n")
+		else:
+			print("Series Draw with Score",human_score,"-",computer_score)a
